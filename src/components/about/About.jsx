b556ea4735ce86c4ce80e351be88e65ab5d63646
Willian Rosa Ramirez <willianrosaramirez@MacBook-Pro-de-Willian.local>
2022-08-22 23:57:51 -0600

    finishing components of ABOUT and EXPERIENCE

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,30 +13,44 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          <div className="about_me-image">
+          <div className="about__me-image">
             <img src={ME} alt="image about me" />
           </div>
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className="about_card">
+            <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>1+ Years Working</small>
             </article>
-            <article className="about_card">
+            <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>4+ WorldWide</small>
             </article>
 
-            <article className="about_card">
+            <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
               <small>4+ Completed</small>
             </article>
           </div>
-          <p>lorem60</p>
+          <p>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
+            de las industrias desde el año 1500, cuando un impresor (N. del T.
+            persona que se dedica a la imprenta) desconocido usó una galería de
+            textos y los mezcló de tal manera que logró hacer un libro de textos
+            especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
+            como texto de relleno en documentos electrónicos, quedando
+            esencialmente igual al original. Fue popularizado en los 60s con la
+            creación de las hojas "Letraset", las cuales contenian pasajes de
+            Lorem Ipsum, y más recientemente con software de autoedición, como
+            por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem
+            Ipsum.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { FaCheckSquare } from "react-icons/fa";
 
 function Experience() {
   return (
-    <section id='experience'>Experience</section>
-  )
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>FrontEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Angular</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Angular Material</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>BackEnd Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                {" "}
+                <h4>Express</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>Mongo DB</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaCheckSquare className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Experience
- 
+export default Experience;
